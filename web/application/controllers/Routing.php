28c7260f0e253771c8538132a1e4471e3aa28fb9
imaduddinharis <imaduddinhariss@gmail.com>
2019-11-23 12:37:14 +0700

    routing

---
+++ web/application/controllers/Routing.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('Res.php');
+
+Class Routing extends CI_Controller{
+
+    var $API ="";
+    var $asset ="";
+
+    function __construct() {
+        parent::__construct();
+        
+        /* API Host */
+        $res = new Res();
+        $this->API = $res->getApi();
+
+        /* Library */
+        $this->load->library('session');
+        $this->load->library('curl');
+        
+        /* Helper */
+        $this->load->helper('form');
+        $this->load->helper('url');
+    }
+
+    // menampilkan data kontak
+    function index(){
+        redirect(base_url().'dashboard');
+    }
+}
