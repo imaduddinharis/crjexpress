8adf3bb9ce63d2e72f206cee1ecd0e42bbbe48e4
imaduddinharis <imaduddinhariss@gmail.com>
2019-12-05 06:37:23 +0700

    Basic Auth API

--- api/application/controllers/Auth.php
+++ api/application/controllers/Auth.php
@@ -21,15 +21,36 @@ class Auth extends REST_Controller {
     function index_post() {
         $code = '';
         
-        $this->db->where('username', $this->post('username'));
-        $this->db->where('password', md5($this->post('password')));
-        $isValid = $this->db->get('users')->result();
-        if (count($isValid) > 0) {
-            $code = 'BAS1';
-            $this->response(array('status' => $code,'token' => base64_encode($this->post('username').':'.md5($this->post('password').':'.date('His'))), 'result' => $isValid, 200));
-        } else {
+        $headers = $this->input->get_request_header('APIKey');
+        // $this->response(array('status' => $code, 'message' => $this->input->get_request_header(),502));
+        $this->db->where('api_key', $headers);
+        $validation = $this->db->get('platform')->result();
+        if(count($validation) > 0) {     
+            $this->db->where('username', $this->post('username'));
+            $this->db->where('password', md5($this->post('password')));
+            $isValid = $this->db->get('users')->result();
+            if (count($isValid) > 0) {
+                $code = 'BAS1';
+                $this->db->where('username', $this->post('username'));
+                $this->db->select('employees.id_employees,
+                                    employees.position,
+                                    employees.branch_office,
+                                    employees_detail.fullname,
+                                    employees_detail.address,
+                                    employees_detail.phone_number,
+                                    employees_detail.photo
+                                ');
+            $this->db->from('employees');
+            $this->db->join('employees_detail', 'employees_detail.id_employees = employees.id_employees', 'left');
+            $user_info = $this->db->get()->result();
+            $this->response(array('status' => $code,'token' => base64_encode($this->post('username').':'.md5($this->post('password').':'.date('His'))), 'result' => array('account'=>$isValid,'detail'=>$user_info), 200));
+            } else {
+                $code = 'BAS0';
+                $this->response(array('status' => $code, 'message' => 'Username or Password incorrect',502));
+            }
+        }else{
             $code = 'BAS0';
-            $this->response(array('status' => $code, 502));
+            $this->response(array('status' => $code, 'message' => 'Authentication Failed',502));
         }
     }
 
